f561cd8c747be1575d224ce32fe18ffa26b109e4
Diego Castelli <Diego Castelli@Diego>
2017-08-01 20:45:28 -0300

    Agregue temas para mejorar la interfaz

--- MailClient/src/Grafica/Ventanas.java
+++ MailClient/src/Grafica/Ventanas.java
@@ -1,22 +1,31 @@
 package Grafica;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.Timer;
+import javax.swing.*;
 import javax.swing.JTextPane;
 import java.awt.Font;
-import javax.swing.DropMode;
 import javax.swing.UIManager;
 
-import logica.FachadaLog;
-
-import persistencia.Configuracion;
 
 public class Ventanas {
 
 	public static void main(String[] args) throws InterruptedException {
 		
+		
+		try{
+			  
+			  JFrame.setDefaultLookAndFeelDecorated(true);
+			  JDialog.setDefaultLookAndFeelDecorated(true);
+			  //  UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+			  //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			  UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); // el que mas me gusto
+			 // UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+			 
+			}
+			catch (Exception e)
+			 {
+			  e.printStackTrace();
+			 }
 		// TODO Auto-generated method stub
 		Presentacion present = new Presentacion();
 		present.setLocationRelativeTo(null);  
