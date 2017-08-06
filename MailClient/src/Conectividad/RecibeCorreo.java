773f00e8c29e393b2741b6c45ab97d7950275e54
alkuy <alkuy@hotmail.com>
2017-08-06 19:15:43 -0300

    Correos entrantes a la bandeja de entrada

--- MailClient/src/Conectividad/RecibeCorreo.java
+++ MailClient/src/Conectividad/RecibeCorreo.java
@@ -1,6 +1,7 @@
 package Conectividad;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.Properties;
  
 import javax.mail.Folder;
@@ -13,11 +14,14 @@ import javax.mail.URLName;
 import com.icegreen.greenmail.user.UserException;
 import com.icegreen.greenmail.util.ServerSetupTest;
 
+import logica.FachadaLog;
+
 public class RecibeCorreo {
 	
 	private static final String LOCALHOST = "127.0.0.1";
 	private String NomUsuario;
 	private String PassUsu;
+	FachadaLog FL = FachadaLog.getInstancia();
 
 	
 	public RecibeCorreo(String NomUsu, String PassUsu){
@@ -63,8 +67,8 @@ public class RecibeCorreo {
 
 
 	public void getMails() throws IOException, MessagingException, UserException, InterruptedException {
-		 
-	    	int contador, i;
+		 	String Asunto, Recibidos, Emisor, Receptor;
+	    	int contador, i, IdConv;
 	        // fetch the e-mail from pop3 using javax.mail ..
 	        Properties props = new Properties();
 	        props.setProperty("mail.pop3.connectiontimeout", "6000");
@@ -81,8 +85,19 @@ public class RecibeCorreo {
 	        i = 0;
 	        while (contador > 0){
 	        	Message m = messages[i];
-	        	System.out.println("Nuevo e-mail de " + m.getFrom()[0].toString() + " | " + "Para: " +  m.getHeader("To")[0].toString() + " | " + "Asunto: " +  m.getSubject());
-	        	System.out.println(m.getContent().toString());
+	        	String [] cortarString = m.getSubject().split("%");
+	        	IdConv = Integer.parseInt(cortarString[0]);
+	        	Asunto = cortarString[1];
+	        	Timestamp timestamp = new Timestamp(System.currentTimeMillis()); //Obtengo el momento en que llamo la actualizacion
+				String fecha = timestamp.toString();
+	        	Recibidos = FL.Devuelve_Ruta_Recibidos();
+	        	Emisor = m.getFrom()[0].toString().substring(0, m.getFrom()[0].toString().indexOf("@"));
+	        	Receptor = m.getHeader("To")[0].toString().substring(0, m.getHeader("To")[0].toString().indexOf("@"));
+	        	String [] Edom =  m.getFrom()[0].toString().split("@");
+	        	String [] Rdom =  m.getHeader("To")[0].toString().split("@");
+	        	FL.Guarda_Correo(Recibidos, IdConv, Asunto, Emisor, Edom[1], Receptor, Rdom[1], m.getContent().toString(), fecha);
+//	        	System.out.println("Nuevo e-mail de " + m.getFrom()[0].toString() + " | " + "Para: " +  m.getHeader("To")[0].toString() + " | " + "Asunto: " +  Asunto + " | " + "Id Conversacion: " + IdConv );
+//	        	System.out.println(m.getContent().toString());
 	        	contador--;
 	        	i++;
 	        }
